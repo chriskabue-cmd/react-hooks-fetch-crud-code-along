f4e12b4303a10707487bfd146cbe0b8e3214a3f5
Suleiman <real1sule@gmail.com>
2025-10-28 20:39:00 +0300

    completed fetch crude-code-along

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,33 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => {
+        setItems(items); // Set fetched items to state
+      });
+  }, []);
+
+  function handleAddItem(newItem) {
+    // Assign unique id based on current max id + 1
+    const newItemWithId = { ...newItem, id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1 };
+    setItems([...items, newItemWithId]);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) =>
+      item.id === updatedItem.id ? updatedItem : item
+    );
+    setItems(updatedItems);
+  }
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +46,18 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm  onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id}
+                item={item}
+                onUpdateItem={handleUpdateItem}
+                onDeleteItem={handleDeleteItem} 
+                />
         ))}
       </ul>
     </div>
